fce2c29dc62aa28c10354cb04cf1513a9c8303bc
fuqing <fuqingresl@gmail.com>
2022-05-15 20:01:41 -0400

    icon

--- app/src/main/java/edu/neu/madcourse/numad22sp_qingfu/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/numad22sp_qingfu/MainActivity.java
@@ -3,12 +3,14 @@ package edu.neu.madcourse.numad22sp_qingfu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("MainActivity", "Hello World");
         setContentView(R.layout.activity_main);
     }
 }
